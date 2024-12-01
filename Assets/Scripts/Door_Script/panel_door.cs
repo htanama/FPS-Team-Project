2f1ca023322a249a7fbb552093dd3f89b847af0f
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2024-12-01 17:53:24 -0600

    Door animations troubles
    Got door working, have animations made and set up, unable to trigger and get the door to actually open -- Stuck at the moment taking a break

--- Assets/Scripts/Door_Script/panel_door.cs
+++ Assets/Scripts/Door_Script/panel_door.cs
@@ -1,34 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class panel_door : MonoBehaviour
 {
-    GameObject Door;
-    bool isClosed;
+    bool isOpen = false;  // false = closed, true = open
+    bool inRange = false;
 
-
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Action();
+        if (Input.GetButtonDown("Action") && inRange)
+        {
+            ToggleDoor();
+        }
     }
-    void Action()
+
+    void ToggleDoor()
     {
-        // need to open door on key press
-        if (Input.GetButtonDown("Action"))
-        {
-            Door.SetActive(false);
-        }
-        else
-        {
-            Door.SetActive(true);
-        }
+        isOpen = !isOpen;
+        animator.SetBool("DoorState", isOpen);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player") inRange = true;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player") inRange = false;
     }
 }
