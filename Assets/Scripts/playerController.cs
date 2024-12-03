2cd689b8d60e5320f0a7e60052725b8b6bafa3e8
Juan Contreras <juancontreras1011@yahoo.com>
2024-12-03 01:39:05 -0500

    First push

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,7 +7,7 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //game start
     }
 
     // Update is called once per frame
