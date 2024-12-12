c8034c582d570c042f6f876c51f852dcc7ed9768
bLemons89 <169059301+bLemons89@users.noreply.github.com>
2024-12-12 16:40:28 -0600

    base pickup script added with placeholders

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    //Fields
+    //speed powerup?
+    //power?
+    //inventory? (hold more than one flag?)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            // flag?
+
+            // power ups
+
+            // jammie gun/ammo stuff
+        
+            Destroy(gameObject);
+        }
+    }
+}
