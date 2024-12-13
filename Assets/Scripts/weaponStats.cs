cd365863b6211200c84351d1ecf20c6b43cac70d
Juan Contreras <juancontreras1011@yahoo.com>
2024-12-13 18:19:14 -0500

    Created weapon stats scriptable object

---
+++ Assets/Scripts/weaponStats.cs
@@ -0,0 +1,26 @@
+/*
+  Code Author: Juan Contreras
+  Date: 12/13/2024
+  Class: DEV2
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Able to make with right click
+[CreateAssetMenu]       //Can make an instance of this
+
+public class weaponStats : ScriptableObject //Acts as data storage
+{
+    public GameObject model; //Drag weapon model here
+    public int damage;
+    public float shootRate;
+    public int weaponRange;
+    public int ammoCurrent;
+    public int ammoMax;
+
+    public ParticleSystem hitEffect;//Drag weapon hit effect here
+    public AudioClip[] shootingSounds;  //As an array to choose different sounds
+    public float weaponSoundVolume;
+}
