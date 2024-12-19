780a1fc099a804ee850c68f85e3f7684780b88e4
Juan Contreras <juancontreras1011@yahoo.com>
2024-12-19 00:16:28 -0500

    Commenting and enemies
    spawner has detailed comments and enemies added to the level and started debugging enemy scripts

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] GameObject objectToSpawn;
-    [SerializeField] int numToSpawn;
-    [SerializeField] int timeBetweenSpawns;
-    [SerializeField] Transform[] spawnPos;
+    [SerializeField] GameObject objectToSpawn;  //what to spawn
+    [SerializeField] int numToSpawn;            //how many to spawn
+    [SerializeField] int timeBetweenSpawns;     //wait time between spawns
+    [SerializeField] Transform[] spawnPos;      //array to hold spawn positions
+    [SerializeField] bool onePerPos;            //if true, limits one object per position
 
-    int spawnCount = 0;
+    int spawnCount = 0;                         //keep of how many objects have spawned so far
 
-    bool startSpawning;
-    bool isSpawning;
-    bool[] objectSpawned;
+    bool startSpawning;                         //gives the green light to start spawning
+    bool isSpawning;                            //while true means the spawner is not done spawning
+    bool[] objectSpawned;                       //true if that specific position has spawned the object once
 
     public int NumToSpawn => numToSpawn;     //Read-only getter
     public int SpawnCount => spawnCount;     //Read-only getter
@@ -22,21 +23,21 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //GameManager.instance.UpdateOrbsCollected(numToSpawn);
-
+        //sets the size to how ever many spawn positions are stored
         objectSpawned = new bool[spawnPos.Length];
     }
 
     // Update is called once per frame
     void Update()
     {
+        //spawns without overlapping spawning of other objects
         if (startSpawning && (spawnCount < numToSpawn) && !isSpawning)
         {
-            StartCoroutine(spawn());
+            StartCoroutine(spawn());        //start spawning
         }
-        else if(spawnCount == numToSpawn)
+        else if(spawnCount == numToSpawn)       //when done spawning
         {
-            GameManager.instance.orbsSpawned();
+            GameManager.instance.orbsSpawned();     //tells game manager how many orbs are in the scene
         }
     }
 
@@ -44,27 +45,31 @@ public class Spawner : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            startSpawning = true;
+            startSpawning = true;       //making sure there is a player before spawning
         }
     }
 
     IEnumerator spawn()
     {
+        //starts spawning phase
         isSpawning = true;
-
+        //cooldown between spawns
         yield return new WaitForSeconds(timeBetweenSpawns);
-
+        //get a random position to spawn at from the ones stored
         int spawnInt = Random.Range(0, spawnPos.Length);
-
+        //condition used when spawning at a position only once
         if (objectSpawned[spawnInt] == false)
         {
+            //instantiates the object at the random spawn position
             Instantiate(objectToSpawn, spawnPos[spawnInt].position, spawnPos[spawnInt].rotation);
-
-            objectSpawned[spawnInt] = true;
-
+            //if true only one object per position spawns (number to spawn should be less than the number of positions in this case)
+            if(onePerPos)
+                objectSpawned[spawnInt] = true;     //if true, means that position spawned an object
+            //increment number of objects spawned
             spawnCount++;
-        }
 
+        }
+        //spawning phase is done
         isSpawning = false;
     }
 }
