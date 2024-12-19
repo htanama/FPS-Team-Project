73a09b0f8670c47488f303ad416d402e0928a71a
Harry <harry.tanama@gmail.com>
2024-12-18 21:47:12 -0500

    Revert "health item added, need to fix trigger/collision someone pushed a broken build, so when I pulled/started over there were new bugs that weren't there before"
    This reverts commit 6170d455dadf95d89ae2ce0cdb3758949ed5f6c4.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -41,8 +41,9 @@ public class playerController : MonoBehaviour, IDamage, IOpen
     [SerializeField][Range(0, 20)] private float playerMaxHealth;
     [SerializeField][Range(0, 20)] private float playerCurrentHealth;
     [SerializeField] Image playerHealthBar;
-    //[SerializeField] float fillSpeed;
-    //SerializeField] Gradient colorGradient;
+    [SerializeField] float fillSpeed;
+    [SerializeField] Gradient colorGradient;
+
 
     [SerializeField][Range(1,  10)] int speed;      //Range adds a slider
     [SerializeField][Range(2,  5)]  int sprintMod;
@@ -310,6 +311,10 @@ public class playerController : MonoBehaviour, IDamage, IOpen
         Debug.Log($"Add weapon: {gunList[gunListpos].model.name} and Index= {gunListpos} and length {gunList.Count}");
     }
 
+    // somewhere around this section
+    // jammie add get gun stats
+    // jammie add select gun scroll wheel (want to do a radial menu eventually)
+    // jammie add change gun
 
     void selectGun()
     {     
@@ -501,11 +506,4 @@ public class playerController : MonoBehaviour, IDamage, IOpen
         }
         isPlayingStep = false;
     }
-
-    public void HealthItemPickup(healthItemPickup item)
-    {
-        playerCurrentHealth += item.HealAmount;
-    }
-
-
 }
